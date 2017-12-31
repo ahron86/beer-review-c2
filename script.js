6cd2227a484b11e097ac309b3ee303dffdf51bda
Ahron Cohen <ahron86@hotmail.com>
2017-12-31 11:36:28 +0200

    new changes

--- script.js
+++ script.js
@@ -1,46 +1,25 @@
-var beers = [];
-var clicks = 0;
-
-//Post Button
-$(".post-beer").click(function () {
-    addBeer($(".beer-input").val(), $(".category-input").val(), $('#dropDownId').val());
-    renderBeers();
-})
-
-//$(".sort-rating").click(sortBeers);
-// Sorting button adds 1 to click variable each time it's clicked
-$(".sort-rating").click(function () {
-    sortBeers();
-    clicks ++;
-})
-
-function addBeer(name, category, rating) {
-    beers.push({ "beerName": name, "beerCategory": category, "beerRating": rating });
-}
-
-function clearList() {
-    $('.beers-list').empty();
-}
-
-function renderBeers() {
-    clearList();
-    for (var i = 0; i < beers.length; i++) {
-        $(".beers-list").append("<li>" + "Beer Name: " + beers[i].beerName + " Beer Category: " + beers[i].beerCategory + " Beer Rating : " + beers[i].beerRating + "</li>")
+var BeerReviewApp = function () {
+    var beers = [];
+  
+    var $beerList = $('.beers-list');
+  
+    var updateBeers = function () {
+      $beerList.empty();
+  
+      for (var i = 0; i < beers.length; i += 1) {
+        var beerHTML = "<li>" + beers[i].name + " - " + beers[i].category + "</li>";
+  
+        $beerList.append(beerHTML);
+      }
+    };
+  
+    var addBeer = function (beerName, beerCategory) {
+      beers.push({ name: beerName, category: beerCategory });
+    };
+  
+    return {
+      updateBeers: updateBeers,
+      addBeer: addBeer
     }
-}
-
-function sortBeers() {
-    if (clicks % 2 == 0) {
-        beers.sort(function (a, b) {
-            return (a.beerRating) - (b.beerRating);
-        });
-
-    } else {
-        beers.sort(function (a, b) {
-            return (b.beerRating) - (a.beerRating);
-        });
-    }
-
-    renderBeers();
-}
+  };
 
